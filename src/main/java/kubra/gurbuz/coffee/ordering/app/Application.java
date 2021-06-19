d73434b0ebdfe32daae04bcbff58a153ffc7f583
kubra.gurbuz@etiya.com <kubra.gurbuz@etiya.com>
2021-06-20 00:56:34 +0300

    Create project

---
+++ src/main/java/kubra/gurbuz/coffee/ordering/app/Application.java
@@ -0,0 +1,13 @@
+package kubra.gurbuz.coffee.ordering.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
